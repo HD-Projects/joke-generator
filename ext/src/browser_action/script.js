bf2e95a788b8c56843b41a2d649575a51929d72f
h9839 <60358181+h9839@users.noreply.github.com>
2020-07-23 12:06:56 -0600

    Create script.js

---
+++ ext/src/browser_action/script.js
@@ -0,0 +1,27 @@
+var jokeAPI = "https://official-joke-api.appspot.com/random_joke";
+   var myPromise = new Promise(showJoke);
+ 
+      function showJoke(resolve, reject) {
+      $.ajax({
+        url: jokeAPI,
+        success: resolve,
+ error: reject
+        
+      });
+      }
+        
+      function rejected(){
+$("#setup").text("joke not found");
+ $("#punchline").text("API error"); 
+
+}
+ 
+       
+        function fulfilled(response){
+console.log(response)
+$("#setup").text(response.setup);
+ $("#punchline").text(response.punchline);         
+          
+        }
+      
+       myPromise.then(fulfilled, rejected)
